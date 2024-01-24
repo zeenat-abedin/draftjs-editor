35dab250f8353ddcd4cad40afba356abfc6e6825
zeenat-abedin <zeenu.23@gmail.com>
2024-01-24 20:18:47 +0530

    Update dependencies and add a rich text editor component

--- src/App.js
+++ src/App.js
@@ -1,5 +1,12 @@
+import RichTextEditor from "./components/RichTextEditor";
+
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div>
+      <h2>React wysiwyg Rich text Editor using Draft.js</h2>
+      <RichTextEditor />
+    </div>
+  );
 }
 
 export default App;
