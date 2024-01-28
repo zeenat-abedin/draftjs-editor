a35d921461c54c0dc5c5cd90cd87306c1886cc4f
zeenat-abedin <zeenu.23@gmail.com>
2024-01-28 21:29:04 +0530

    add required changes

--- src/components/CustomEditor.jsx
+++ src/components/CustomEditor.jsx
@@ -1,5 +1,12 @@
 import { useState, useEffect } from "react";
-import { Editor, EditorState, RichUtils, convertToRaw } from "draft-js";
+import {
+  Editor,
+  EditorState,
+  Modifier,
+  RichUtils,
+  convertToRaw,
+  getDefaultKeyBinding,
+} from "draft-js";
 import { convertToHTML } from "draft-convert";
 import "draft-js/dist/Draft.css";
 
@@ -15,11 +22,43 @@ export default function CustomEditor() {
   const handleKeyCommand = (command, editorState) => {
     const newState = RichUtils.handleKeyCommand(editorState, command);
 
+    // If enter key is pressed, than make it normal text
+    if (command === "split-block") {
+      const contentState = editorState.getCurrentContent();
+      const selectionState = editorState.getSelection();
+      const newContentState = Modifier.setBlockType(
+        contentState,
+        selectionState,
+        "unstyled"
+      );
+      const newEditorState = EditorState.push(
+        editorState,
+        newContentState,
+        "split-block"
+      );
+      setEditorState(newEditorState);
+      return "handled";
+    }
+
     if (newState) {
       setEditorState(newState);
       return "handled";
     }
     return "not handled";
+  };
+
+  const handleReturn = (e) => {
+    // Handle Return key to create a new line
+    e.preventDefault();
+    const newEditorState = EditorState.push(
+      editorState,
+      Modifier.splitBlock(
+        editorState.getCurrentContent(),
+        editorState.getSelection()
+      )
+    );
+    setEditorState(newEditorState);
+    return "handled";
   };
 
   const styleMap = {
@@ -35,6 +74,49 @@ export default function CustomEditor() {
     localStorage.setItem("editorData", serializedData);
   };
 
+  const handleBeforeInput = (char, editorState) => {
+    const currentContent =
+      editorState.getCurrentContent().getPlainText("") + char;
+
+    //  If currentContent starts with # + space, change block type to header-one, and remove # + space from text
+    if (currentContent === "# ") {
+      setEditorState(RichUtils.toggleBlockType(editorState, "header-one"));
+      return "handled";
+    }
+
+    // If currentContent starts with * + space , then make it bold
+    if (currentContent === "* ") {
+      setEditorState(RichUtils.toggleInlineStyle(editorState, "BOLD"));
+      return "handled";
+    }
+
+    // If currentContent starts with ** + space , then make it red line
+    if (currentContent === "** ") {
+      setEditorState(RichUtils.toggleInlineStyle(editorState, "RED"));
+      return "handled";
+    }
+
+    // If currentContent starts with *** + space , then make it underline
+    if (currentContent === "*** ") {
+      setEditorState(RichUtils.toggleInlineStyle(editorState, "UNDERLINE"));
+      return "handled";
+    }
+
+    // If enter key is pressed, than make it normal text
+    if (char === "Enter") {
+      setEditorState(RichUtils.toggleInlineStyle(editorState, "unstyled"));
+      return "handled";
+    }
+  };
+
+  const keyBindingFn = (e) => {
+    console.log("e.keyCode", e.keyCode);
+    // If enter key is pressed, than make it normal text
+    if (e.keyCode === 13) {
+      return "split-block";
+    }
+    return getDefaultKeyBinding(e);
+  };
   useEffect(() => {
     handleSave();
   }, [editorState]);
@@ -56,10 +138,15 @@ export default function CustomEditor() {
         <div className={styles.draftEditor}>
           <Editor
             editorState={editorState}
-            onChange={setEditorState}
+            onChange={(editorState) => {
+              setEditorState(editorState);
+            }}
             handleKeyCommand={handleKeyCommand}
             placeholder="Write something!"
             customStyleMap={styleMap}
+            handleBeforeInput={handleBeforeInput}
+            keyBindingFn={keyBindingFn}
+            handleReturn={handleReturn}
           />
         </div>
       </div>
